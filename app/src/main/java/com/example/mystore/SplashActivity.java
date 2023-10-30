7c7ae6174733ac83a9dedb72464cc9f810343cb7
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2023-10-29 19:27:24 -0600

    Diseño Login completo

--- app/src/main/java/com/example/mystore/SplashActivity.java
+++ app/src/main/java/com/example/mystore/SplashActivity.java
@@ -1,7 +1,12 @@
 package com.example.mystore;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.mystore.login.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        LottieAnimationView lav = findViewById(R.id.lavAnimacionStore);
+
+        lav.addAnimatorListener(
+                new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+
+                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
